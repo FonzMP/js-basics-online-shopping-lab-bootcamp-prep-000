9c8b9d9257dd9fec5768094328cb96892231d7f9
Alex Wilson <fonzmp@gmail.com>
2018-04-18 03:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     let currentItemName = cart[i].itemName;
-    const itemLowered = item;
     console.log(currentItemName);
     if (currentItemName === item) {
       delete cart[i];
