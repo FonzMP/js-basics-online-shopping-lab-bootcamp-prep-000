91e2cbdf911a8de9591c65683634a4ac5cbfa2ab
Alex Wilson <fonzmp@gmail.com>
2018-04-18 16:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   const totalPrice = total();
+  const creditCardNumber = cardNumber;
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
