b3de917e78de54b7e7698f3099ea9071a3e608ee
Alex Wilson <fonzmp@gmail.com>
2018-04-18 16:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,6 +81,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (!cardNumber) {
-    return "Sorry, we can't process your order";
+    return "Sorry, we don't have a credit card on file for you.";
   }
 }
