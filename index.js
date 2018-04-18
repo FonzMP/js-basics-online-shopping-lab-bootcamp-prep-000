d266a6488b11071971a844a12627b5edbe094fe7
Alex Wilson <fonzmp@gmail.com>
2018-04-18 04:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ function removeFromCart(item) {
     let currentItem = cart[i];
     if (currentItem === item) {
       let removed = cart.splice(i, 1);
+    } else {
+      return "That item is not in your cart.";
     }
   }
   return cart;
