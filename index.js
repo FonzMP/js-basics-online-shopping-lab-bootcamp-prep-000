f990b612a00b0f59aeaabb13d2afbfc1f475643d
Alex Wilson <fonzmp@gmail.com>
2018-04-18 16:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,5 +82,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    return "Your total cost is $" + total() + ", which will be charged to the card 93296759."
   }
 }
