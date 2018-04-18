1c5b0a53e158d97bd723ef0b85c20422aaf8adaa
Alex Wilson <fonzmp@gmail.com>
2018-04-18 03:59:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,7 @@ function removeFromCart(item) {
   // write your code here
   let message = "";
   for (let i = 0; i < cart.length; i++) {
-    let currentItemName = cart[i].itemName;
-    console.log(currentItemName);
+    console.log(cart[i]);
   }
   return message;
 }
