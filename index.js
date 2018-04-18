438e9ffc74b83625b49bf45a75502065a8dba1cd
Alex Wilson <fonzmp@gmail.com>
2018-04-18 16:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function removeFromCart(item) {
   let message = "";
   for (let i = 0; i < cart.length; i++) {
     let currentItem = cart[i];
-    let secondary = parseInt(cart[i]) + 1;
+    let secondary = parseInt(i + 1);
     console.log(secondary);
     if (currentItem === item) {
       let removed = cart.splice(i, i);
