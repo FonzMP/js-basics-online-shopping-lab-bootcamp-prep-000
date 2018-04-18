1374466f7b944a29d80287f69fda7c5898717092
Alex Wilson <fonzmp@gmail.com>
2018-04-18 03:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ function total() {
   let total = 0;
   for (let i = 0; i < cart.length; i++) {
     total += total + cart[i].itemPrice;
+    console.log(cart[i].itemPrice);
   }
   return total;
 }
