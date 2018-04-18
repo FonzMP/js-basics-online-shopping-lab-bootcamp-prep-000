d88e9e678d2796c3bd185c5e0f95fe5f2a524e7b
Alex Wilson <fonzmp@gmail.com>
2018-04-18 16:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
     let secondary = parseInt(i + 2);
     console.log(secondary);
     if (currentItem === item) {
-      let removed = cart.splice(i, secondary);
+      let removed = cart.splice(i, i++);
       console.log(removed);
       return cart;
     } else {
